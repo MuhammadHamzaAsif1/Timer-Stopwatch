37d45932e488edade95831f716d6dc039559718c
MuhammadHamza1 <170727264+MuhammadHamzaAsif1@users.noreply.github.com>
2026-01-22 11:21:59 +0500

    wrote the counterIncrementor() function that increments seconds variable, then minutes when seconds == 60, then hours when minutes == 60. then displays the result on index.html

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let minutes = 0;
 let hours = 0;
 let timerId = null;
 
+function counterIncrementer(){
+    seconds++;
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+    }
+    if (minutes == 60) {
+        minutes = 0;
+        hours++;
+    }
+    document.getElementById("timer").textContent = 
+    `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
